6add02f8e6a614fb1447351eb1c5d11ae2d60e25
Yperumal <yperumal@pm.me>
2021-03-25 09:29:13 +0200

    setup R

---
+++ setup git.R
@@ -0,0 +1,11 @@
+library(usethis)
+git_sitrep()
+
+usethis::use_git_config(
+    user.name = "Yperumal",
+    user.email = "yperumal@pm.me")
+
+usethis::create_github_token()
+fde62eb2580fd0c707b224c7c0a7125009119b5e
+
+usethis::edit_r_environ()
